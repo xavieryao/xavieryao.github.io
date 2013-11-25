fb565906eb3faacb961df2860fec4bab065dc8c1
Xavier Yao <xavieryao@live.cn>
2013-11-25 12:22:39 +0800

    Content table fixed.

--- javascripts/page_switch.js
+++ javascripts/page_switch.js
@@ -23,7 +23,7 @@ function loadContentTable(){
 		ct = eval(result);
 		var md = "###Table Of Content   \n         \n";
 		for(var i = 0; i < ct.length; i++){
-			md = md.concat('<a href="javascript:switchPage("'+ct[i].URL+'")" >'+ct[i].title+'</a>   \n');
+			md = md.concat("<a href=\"javascript:switchPage('"+ct[i].URL+"')\" >"+ct[i].title+'</a>   \n');
 		}
 		reloadFromMd(md);
 	});
