8cc9dd006eed093b76a5d626a28baa6deed0ae6b
Xavier Yao <xavieryao@live.cn>
2014-01-13 21:04:50 +0800

    简易服务器，<480px时的banner搞定

---
+++ server.js
@@ -0,0 +1,18 @@
+/*
+ * A temperary web server in Node.js for mobile test.
+ */
+
+var http = require("http");
+var fs = require('fs');
+
+http.createServer(function (req,res){
+	if(req.url == '/')
+		req.url = '/index.html';	
+	req.url = '.' + req.url;
+	console.log(req.url);
+	fs.readFile(req.url,'utf-8',function(err,data){
+		res.writeHead(200);
+		res.write(data);
+		res.end();
+	});
+}).listen(3000);
